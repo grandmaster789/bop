d69701981a2b9949945935c2f41169c7ba45eff5
grandmaster789 <mail@grandmaster.nu>
2022-04-02 23:08:48 +0200

    removed unused metaprogramming stuff got the testing framework actually working

--- tests/test.cpp
+++ tests/test.cpp
@@ -13,12 +13,3 @@ unsigned int factorial(unsigned int number) {
 TEST_CASE("testing", "[factorial]") {
     REQUIRE(factorial(1) == 1);
 }
-
-/*
-TEST_CASE("Factorials are computed", "[factorial]") {
-    REQUIRE(Factorial(1) == 1);
-    REQUIRE(Factorial(2) == 2);
-    REQUIRE(Factorial(3) == 6);
-    REQUIRE(Factorial(10) == 3628800);
-}
-*/
