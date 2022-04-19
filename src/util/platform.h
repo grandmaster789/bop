1963ae7fad5330c8e93ea9720e9da3004a0be6de
Gijsbert dos Santos <gijs@aeroscan.nl>
2022-04-19 11:06:15 +0200

    added reference document some small platform identification code fixed up variants in co_job_promise

---
+++ src/util/platform.h
@@ -0,0 +1,29 @@
+#pragma once
+
+// simple platform detection
+#define BOP_PLATFORM_WINDOWS 1
+#define BOP_PLATFORM_LINUX   2
+
+// may be oversimplified, but mostly true
+#ifdef _WIN32
+	#define BOP_PLATFORM BOP_PLATFORM_WINDOWS
+#else
+	#define BOP_PLATFORM BOP_PLATFORM_LINUX
+#endif
+
+#ifdef _DEBUG
+	#define BOP_DEBUG
+#endif
+
+namespace bop {
+	enum class e_Platform {
+		windows = BOP_PLATFORM_WINDOWS,
+		linux   = BOP_PLATFORM_LINUX,
+
+		current = BOP_PLATFORM
+	};
+
+	static constexpr e_Platform k_Platform = e_Platform::current;
+
+	// maybe add iostream?
+}
